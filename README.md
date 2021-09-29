1f80d47c3b044ce44ba1a7be48cd7e91ee9df192
kalew515 <ilovewk51@gmail.com>
2021-09-30 00:40:43 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
 # bupt-clockIn
-北邮疫情自动打卡脚本
+北邮疫情自动打卡，支持每日定时打卡，微信推送打卡状态
+
+如果觉得有用就点个⭐吧～
+
+## Framework
+
+├── **clockIn**               // 代码及配置文件目录
+│  ├── clockIn.py         // 打卡脚本，只会执行一次，需要配合类似`cron`等具有支持定时功能的服务实现每日打卡
+│  ├── clockIn_timing.py   // 无需定时服务支持，支持每日打卡
+│  └── config.json        // 配置文件
+├── docker-compose.yml     // docker-compose配置
+├── LICENSE
+├── README.md 
+└── requirements.txt       // 相关依赖
+
+## How To Use
+
+### 相关准备
+
+首先拉取项目：
+
+`git clone https://github.com/KaleW515/bupt_clockIn.git`
+
+项目目录中的`config.json`文件是配置文件，首先需要修改配置文件：
+
+```json
+{
+  "username": "",
+  "password": "",
+  "time": "09:30",
+  "appToken": "",
+  "uid": ""
+}
+```
+
+在`username`后面填上自己的学号，在`password`后面填上自己的密码，`time`可以修改默认自动打卡的时间，默认是`9：30`，如修改为`08：30`则是每天`8：30`进行打卡，这里注意如果时间只有一位数一定要在前面添加`0`，比如`8：30`是错误的，应该改为`08：30`
+
+### 微信推送
+
+如果不需要微信推送服务，则可以忽略该节。
+
+本项目的微信推送用到了[WxPusher](https://github.com/wxpusher/wxpusher-client)，微信推送需要用到`config.json`配置文件中的`appToken`和`uid`字段。
+
+1. 根据 [WxPusher](https://wxpusher.zjiecode.com/docs/#/)的文档首先注册并且创建应用，注册时会提示`appToken`，记录到`config.json`文件的`appToken`字段中
+
+2. 创建完成后扫码关注应用，根据文档说明拿到自己的`uid`填到`config.json`文件中的`uid`字段中
+
+3. 微信推送服务配置完成
+
+   例如：
+
+   ```json
+   {
+     "username": "2021xxxxxx",
+     "password": "xxxxxxxxxx",
+     "time": "09:30",
+     "appToken": "AT_xxxxxxxxxxxxxxxxxxxx",
+     "uid": "UID_xxxxxxxxxxxxxxxxxxxx"
+   }
+   ```
+
+### 开始运行
+
+项目提供了两种打卡的实现方式，项目更推荐第二种方式
+
+#### 使用`cron`配合`clockIn.py`进行每日打卡
+
+- 安装所需依赖：
+
+  `pip install -r requirements.txt`
+
+- 设置定时任务，例如
+
+  `30 9 * * * python /home/kale/bupt_clockIn/clockIn.py`
+
+  即可在每日的9点30分进行自动打卡
+
+#### 使用`docker`完成脚本部署
+
+- 项目根目录下有`docker-compose.yml`文件，输入命令：
+
+  `docker-compose up`
+
+  保持容器后台运行，即可实现每日打卡。**注意关机之后要重新启动执行命令启动容器**
+
+- 也可以直接运行`clockIn_timing.py`文件，也能实现每日自动打卡
---
+++ clockIn/clockIn.py
@@ -0,0 +1,106 @@
+import json
+import re
+import time
+import os
+
+import requests
+import schedule
+from bs4 import BeautifulSoup
+
+
+class ClockIn:
+    __cookie = ""
+    __username = ""
+    __password = ""
+    __login_url = "https://app.bupt.edu.cn/uc/wap/login/check"
+    __upload_url = "https://app.bupt.edu.cn/ncov/wap/default/save"
+
+    def __init__(self, _username, _password):
+        self.__username = _username
+        self.__password = _password
+
+    def __get_cookie(self):
+        headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 "
+                          "Safari/537.36"
+        }
+        data = {
+            "username": self.__username,
+            "password": self.__password
+        }
+        res = requests.post(url=self.__login_url, headers=headers, data=data)
+        for key, value in res.cookies.items():
+            self.__cookie += key + "=" + value + "; "
+        return self.__cookie[:-2]
+
+    def __get_html(self):
+        self.__get_cookie()
+        url = "https://app.bupt.edu.cn/ncov/wap/default/index?from=history"
+        headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 "
+                          "Safari/537.36",
+            "cookie": self.__cookie
+        }
+        res = requests.get(url=url, headers=headers)
+        return res.text
+
+    def __get_old_info(self):
+        res = BeautifulSoup(self.__get_html(), "html.parser")
+        script = list(res.select("script[type='text/javascript']"))[1]
+        pattern = re.compile(r"oldInfo: {.*?},$", re.MULTILINE | re.DOTALL)
+        s = json.loads(re.findall(pattern, str(script))[0][9:-1])
+        s["created"] = int(time.time())
+        s["date"] = time.strftime("%Y%m%d")
+        return s
+
+    def save(self):
+        jsonobj = self.__get_old_info()
+        headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 "
+                          "Safari/537.36",
+            "cookie": self.__cookie
+        }
+        res = requests.post(url=self.__upload_url, headers=headers, data=jsonobj)
+        return res.text
+
+
+def upload(username, password):
+    clock_in = ClockIn(username, password)
+    msg = clock_in.save()
+    return msg
+
+
+def push_msg(msg: str, js: json):
+    push_url = "http://wxpusher.zjiecode.com/api/send/message"
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = {
+        "appToken": js["appToken"],
+        "content": msg,
+        "contentType": 1,
+        "uids": [js["uid"]],
+    }
+    if js["appToken"] == "" or js["uid"] == "":
+        return
+    res = requests.post(url=push_url, headers=headers, json=data)
+    return res
+
+
+def main():
+    with open(os.path.dirname(__file__) + "/config.json", "r") as f:
+        data = json.load(f)
+    for i in range(3):
+        time.sleep(i * 5)
+        msg = upload(data["username"], data["password"])
+        if json.loads(msg)["m"] == "今天已经填报了" or json.loads(msg)["m"] == "操作成功":
+            print(time.strftime("%Y-%m-%d %H:%M:%S") + " " + json.loads(msg)["m"])
+            push_msg(time.strftime("%Y-%m-%d %H:%M:%S") + " " + json.loads(msg)["m"], data)
+            return
+        else:
+            print(time.strftime("%Y-%m-%d %H:%M:%S") + " " + json.loads(msg)["m"])
+            push_msg(time.strftime("%Y-%m-%d %H:%M:%S") + " " + json.loads(msg)["m"], data)
+
+
+if __name__ == '__main__':
+    main()
