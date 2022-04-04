9f622fc483fbb8fb2baa1302d9a4e345c128ff19
Xuewei Niu <justxuewei@apache.org>
2022-04-04 11:02:42 +0800

    fix: fix multiple users bug

--- clockIn/clockIn_timing.py
+++ clockIn/clockIn_timing.py
@@ -10,14 +10,15 @@ import req_model
 def main(js: json):
     for i in range(3):
         time.sleep(i * 5)
-        if data[item]["username"] != "" and data[item]["password"] != "":
-            msg = req_model.upload(data[item]["username"], data[item]["password"])
+        if js["username"] != "" and js["password"] != "":
+            msg = req_model.upload(js["username"], js["password"])
             if msg == "":
                 print(time.strftime("%H:%M") + " " + "打卡失败！！！！")
-                req_model.push_msg(time.strftime("%H:%M") + " " + "打卡失败！！！！", data[item])
+                req_model.push_msg(time.strftime("%H:%M") + " " + "打卡失败！！！！", js)
             elif json.loads(msg)["m"] == "今天已经填报了" or json.loads(msg)["m"] == "操作成功":
-                print(time.strftime("%H:%M") + " " + json.loads(msg)["m"])
-                req_model.push_msg(time.strftime("%H:%M") + " " + json.loads(msg)["m"], js)
+                msg = '{} {} AT {}'.format(js['username'], json.loads(msg)["m"], time.strftime("%H:%M"))
+                print(msg)
+                req_model.push_msg(msg, js)
                 return
             else:
                 print(time.strftime("%H:%M") + " " + json.loads(msg)["m"])
@@ -25,7 +26,7 @@ def main(js: json):
 
 
 if __name__ == '__main__':
-    with open(os.path.dirname(__file__) + "/config.json", "r") as f:
+    with open(os.path.join(os.path.dirname(__file__), "config.json"), "r") as f:
         data = json.load(f)
     for item in data:
         schedule.every().day.at(data[item]["time"]).do(main, data[item])
